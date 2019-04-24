243defdc4209f7858f7926d80701c44c3bf26938
Matthew McIntyre <mattymac2@gmail.com>
2019-04-24 13:53:53 +0100

    timing adjustments implemented

--- src/reducers/pomoReducer.js
+++ src/reducers/pomoReducer.js
@@ -3,16 +3,20 @@ import {
   SET_BREAK,
   RESET_TO_DEFAULT,
   TICK_SECOND,
-  SET_INTERVAL_ID
+  SET_INTERVAL_ID,
+  SET_REMAINING_TIME
 } from "../actions/actionTypes";
 
 const initialState = {
   //timer default of 25 mins
   //break default of 5 mins
-  secondsRemaining: 25,
-  timer: 25,
+  secondsRemaining: 1500,
+  mm: 25,
+  ss: "00",
+  session: 25,
   break: 5,
-  intervalId: 0
+  intervalId: 0,
+  inSession: true
 };
 
 const pomoReducer = (state = initialState, action) => {
@@ -24,33 +28,103 @@ const pomoReducer = (state = initialState, action) => {
   switch (action.type) {
     //TODO
     case SET_SESSION:
-      return {
-        ...state,
-        timer: action.newTime,
-        secondsRemaining: state.secondsRemaining + 1
-      };
+      //if in session and paused set seconds remaining and mm / ss values
+      if (state.inSession === true && state.intervalId === 0) {
+        return {
+          ...state,
+          session: action.newTime,
+          secondsRemaining: action.newTime * 60,
+          mm: action.newTime,
+          ss: "00"
+        };
+      }
+      //if in session and counting down set seconds remaining and session
+      else if (state.inSession === true && state.intervalId !== 0) {
+        return {
+          ...state,
+          session: action.newTime,
+          secondsRemaining: action.newTime * 60,
+          mm: action.newTime,
+          ss: "00"
+        };
+      }
+      //if not in break just set break
+      else {
+        return {
+          ...state,
+          session: action.newTime
+        };
+      }
 
     //TODO FIX ME
     case SET_BREAK:
-      return {
-        ...state,
-        timer: action.newTime,
-        secondsRemaining: state.secondsRemaining - 1
-      };
+      //if in break and paused set seconds remaining and mm / ss values
+      if (state.inSession === false && state.intervalId === 0) {
+        return {
+          ...state,
+          break: action.newTime,
+          secondsRemaining: action.newTime * 60,
+          mm: action.newTime,
+          ss: "00"
+        };
+      }
+      //if in break and counting down set seconds remaining and break
+      else if (state.inSession === false && state.intervalId !== 0) {
+        return {
+          ...state,
+          break: action.newTime,
+          secondsRemaining: action.newTime * 60,
+          mm: action.newTime,
+          ss: "00"
+        };
+      }
+      //if not in break just set break
+      else {
+        return {
+          ...state,
+          break: action.newTime
+        };
+      }
+
+    case SET_REMAINING_TIME:
+      if (action.sessionOrBreak === true) {
+        return {
+          ...state,
+          secondsRemaining: state.session * 60,
+          inSession: true
+        };
+      } else {
+        return {
+          ...state,
+          secondsRemaining: state.break * 60,
+          inSession: false
+        };
+      }
 
     case RESET_TO_DEFAULT:
       return {
         ...state,
         timer: 25,
-        break: 5
+        break: 5,
+        intervalId: 0,
+        secondsRemaining: 25,
+        inSession: true
       };
 
     case TICK_SECOND:
-      let newTime = state.secondsRemaining - 1;
+      let minusSecond = state.secondsRemaining - 1;
+
+      let m = Math.floor(minusSecond / 60);
+      let s = Math.floor((minusSecond % 60) % 60);
+
+      let mm = m < 10 ? "0" + m : m * 1;
+      let ss = s < 10 ? "0" + s : s * 1;
 
       return {
         ...state,
-        secondsRemaining: newTime
+        secondsRemaining: minusSecond,
+        mm: mm,
+        ss: ss
       };
 
     case SET_INTERVAL_ID:
